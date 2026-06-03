9aaddec7ea5cbc0f713d812d0f8162601f961a90
oniaz <32391049+oniaz@users.noreply.github.com>
2026-06-03 07:08:13 +0300

    feat: add MongoDB connection setup in db.js and integrate it in index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import helmet from "helmet";
 
+import connectDB from "./config/db.js";
+
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +18,8 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+
+await connectDB();
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to the Waste Reduction API!"});
